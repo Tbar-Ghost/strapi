4e8138b157846f7141bae088dea7b203888d0237
Thanat <stampstrk@gmail.com>
2025-05-09 17:14:39 +0700

    upgraphql

--- config/plugins.js
+++ config/plugins.js
@@ -14,3 +14,17 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+module.exports = () => ({
+    graphql: {
+      enabled: true,
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 7,
+        amountLimit: 100,
+      },
+    },
+  });
+  
