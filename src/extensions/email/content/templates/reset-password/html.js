fd75889c48b512f54ee99b36c80119bf3f80e38f
Thanat <stampstrk@gmail.com>
2025-05-14 20:40:49 +0700

    add URL edit

--- src/extensions/email/content/templates/reset-password/html.js
+++ src/extensions/email/content/templates/reset-password/html.js
@@ -1,10 +1,11 @@
 module.exports = ({ user, resetPasswordToken, url }) => {
   const resetLink = `${url}/reset-password?code=${resetPasswordToken}`;
+
   return `
-    <p>Hi ${user.username || user.email},</p>
-    <p>We received a request to reset your password.</p>
-    <p>Click the link below to set a new password:</p>
-    <a href="${resetLink}" target="_blank" rel="noopener noreferrer">${resetLink}</a>
-    <p>If you didn't request this, please ignore this email.</p>
+    <p>Hello ${user.username || user.email},</p>
+    <p>Click the link below to reset your password:</p>
+    <a href="${resetLink}" target="_blank">${resetLink}</a>
+    <p>If you did not request this, please ignore this email.</p>
   `;
 };
+
