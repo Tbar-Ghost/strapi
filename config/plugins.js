9cc88fbc49cde4ba3089944c9c24761769f14fd7
Thanat <stampstrk@gmail.com>
2025-05-19 18:07:54 +0700

    add S3

--- config/plugins.js
+++ config/plugins.js
@@ -33,6 +33,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+    },
+  },
 });
 
 console.log('NODE_ENV', process.env.NODE_ENV);
