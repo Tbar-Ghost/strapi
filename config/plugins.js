cd99d7fa95c3c78e8edd17845d5c008d93a3155a
Thanat <stampstrk@gmail.com>
2025-05-20 16:12:18 +0700

    edit S3 .env add editinstrapi2

--- config/plugins.js
+++ config/plugins.js
@@ -50,14 +50,17 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        s3Options: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
-          params: {
-            Bucket: env('AWS_BUCKET'),
-          },
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
         },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
