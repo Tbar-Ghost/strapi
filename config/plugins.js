bded0daa3fdfc9bc474c240dfdfa05d98df0a08b
Thanat <stampstrk@gmail.com>
2025-05-20 18:38:02 +0700

    edit S3 .env add editinstrapi5

--- config/plugins.js
+++ config/plugins.js
@@ -48,7 +48,7 @@ module.exports = ({ env }) => ({
   // },
   upload: {
     config: {
-      provider: '@strapi/provider-upload-aws-s3',
+      provider: 'aws-s3',
       providerOptions: {
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
