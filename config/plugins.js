1e762626ce8f769458886c1400e899f178dec5c4
Thanat <stampstrk@gmail.com>
2025-06-05 22:14:33 +0700

    config S3 add coslog 10

--- config/plugins.js
+++ config/plugins.js
@@ -51,59 +51,60 @@ module.exports = ({ env }) => ({
   //     },
   //   },
   // },
+  upload: {
     upload: {
-    config: {
       provider: 'aws-s3',
       providerOptions: {
-        credentials: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
+        s3Options: {
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+          },
+          region: env('AWS_REGION'),
         },
-        region: env('AWS_REGION'), // เช่น 'us-east-1', 'ap-southeast-1'
-        endpoint: env('AWS_S3_ENDPOINT'), // เช่น 'https://s3.us-east-1.amazonaws.com'
         params: {
           Bucket: env('AWS_BUCKET'),
         },
       },
     },
   },
-    // upload: {
-    //   config: {
-    //     provider: '@strapi/provider-upload-aws-s3',
-    //     providerOptions: {
-    //       accessKeyId: env('AWS_ACCESS_KEY_ID'),
-    //       secretAccessKey: env('AWS_ACCESS_SECRET'),
-    //       region: env('AWS_REGION'),
-    //       params: {
-    //         Bucket: env('AWS_BUCKET'),
-    //       },
-    //     },
-    //   },
-    // },
-    // upload: {
-    //   config: {
-    //     provider: 'aws-s3',
-    //     providerOptions: {
-    //       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    //       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    //       region: process.env.AWS_REGION,
-    //       endpoint: `https://s3.${env('AWS_REGION')}.amazonaws.com`,
-    //       params: {
-    //         Bucket: process.env.AWS_BUCKET,
-    //       },
-    //     },
-    //   },
-    // },
-    'content-type-builder': {
-      enabled: true
-    }
-  });
+  // upload: {
+  //   config: {
+  //     provider: '@strapi/provider-upload-aws-s3',
+  //     providerOptions: {
+  //       accessKeyId: env('AWS_ACCESS_KEY_ID'),
+  //       secretAccessKey: env('AWS_ACCESS_SECRET'),
+  //       region: env('AWS_REGION'),
+  //       params: {
+  //         Bucket: env('AWS_BUCKET'),
+  //       },
+  //     },
+  //   },
+  // },
+  // upload: {
+  //   config: {
+  //     provider: 'aws-s3',
+  //     providerOptions: {
+  //       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  //       region: process.env.AWS_REGION,
+  //       endpoint: `https://s3.${env('AWS_REGION')}.amazonaws.com`,
+  //       params: {
+  //         Bucket: process.env.AWS_BUCKET,
+  //       },
+  //     },
+  //   },
+  // },
+  'content-type-builder': {
+    enabled: true
+  }
+});
 
-  console.log('NODE_ENV', process.env.NODE_ENV);
+console.log('NODE_ENV', process.env.NODE_ENV);
 
-    console.log('AWS_REGION', process.env.AWS_REGION);
-    console.log('AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID);
-    console.log('AWS_SECRET_ACCESS_KEY', process.env.AWS_SECRET_ACCESS_KEY);
-    console.log('AWS_BUCKET', process.env.AWS_BUCKET);
-    console.log('AWS_S3_ENDPOINT', process.env.AWS_S3_ENDPOINT);
+console.log('AWS_REGION', process.env.AWS_REGION);
+console.log('AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID);
+console.log('AWS_SECRET_ACCESS_KEY', process.env.AWS_SECRET_ACCESS_KEY);
+console.log('AWS_BUCKET', process.env.AWS_BUCKET);
+console.log('AWS_S3_ENDPOINT', process.env.AWS_S3_ENDPOINT);
 
